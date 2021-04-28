0d057b83c5fc21ebdf95c947f0650525d8207f17
Victoria <vica.chevchenco@gmail.com>
2021-04-28 17:26:28 -0500

    Changed breadcrumb

--- inside_playlist.php
+++ inside_playlist.php
@@ -99,9 +99,8 @@ input.invisible {visibility:hidden;}
   <!-- Breadcrumb -->
   <nav aria-label="breadcrumb" class="main-breadcrumb">
     <ol class="breadcrumb">
-      <li class="breadcrumb-item"><a href="index.html">Home</a></li>
-      <li class="breadcrumb-item"><a href="javascript:void(0)">User</a></li>
-      <li class="breadcrumb-item active" aria-current="page">User Profile</li>
+      <li class="breadcrumb-item"><a href="profile.php">User Profile</a></li>
+      <li class="breadcrumb-item active" aria-current="page">User Playlist</li>
     </ol>
   </nav>
   <!-- /Breadcrumb -->
