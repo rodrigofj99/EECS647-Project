fe53b1fe7fa0207588adc6adf81a9a43073a86e1
Victoria <vica.chevchenco@gmail.com>
2021-04-28 20:42:28 -0500

    Fixed nav bar for profile.php

--- profile.php
+++ profile.php
@@ -206,8 +206,6 @@ session_start();
           <!-- Breadcrumb -->
           <nav aria-label="breadcrumb" class="main-breadcrumb">
             <ol class="breadcrumb">
-              <li class="breadcrumb-item"><a href="index.html">Home</a></li>
-              <li class="breadcrumb-item"><a href="javascript:void(0)">User</a></li>
               <li class="breadcrumb-item active" aria-current="page">User Profile</li>
             </ol>
           </nav>
